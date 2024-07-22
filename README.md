9751b60b2619d6c17596625f9ed2754bc78f89e3
Robert Auch <rauch@totalnetsolutions.net>
2024-07-22 17:11:07 -0500

    first commit of buildkt

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # kerberos-tools
 little scripts to make kerberos less painful for me
+
+## buildkt.pl
+while loop to build a full keytab using ktutil.
+Useful for taking a username/password and turning it into a keytab for Wireshark to dissect stuff
---
+++ buildkt.pl
@@ -0,0 +1,52 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+my $ktfile=shift;
+my $upn=shift;
+my $realm=shift;
+my $kvno=shift;
+my $password = join("", @ARGV);
+
+$realm=uc($realm);
+
+open(my $kt, ">", "$ktfile") || die "can't open $ktfile for writing! $!";
+
+print $kt "clear_list\n";
+
+my @parts=split(/@/, $upn);
+my $dom=$parts[1];
+my $usr=$parts[0];
+my $salt=uc($dom)."host".lc($usr);
+
+my @enctypes=("aes256-cts-hmac-sha1-96", "aes128-cts-hmac-sha1-96", "arcfour-hmac");
+foreach my $kvn (($kvno, $kvno-1)) {
+foreach my $enc (@enctypes) {
+    foreach my $domain ((uc($dom), lc($dom))) {
+        foreach my $user ((uc($usr), lc($usr))) {
+            print $kt "addent -password -p $user/$domain\@$realm -k $kvno -e $enc\n";
+            print $kt "$password\n";
+            print $kt "addent -password -p $user/".lc($domain)."\@$realm -k $kvno -e $enc\n";
+            print $kt "$password\n";
+            print $kt "addent -password -p $user/$domain\@$realm -k $kvno -e $enc -s $salt\n";
+            print $kt "$password\n";
+            print $kt "addent -password -p $user/".lc($domain)."\@$realm -k $kvno -e $enc -s $salt\n";
+            print $kt "$password\n";
+            print $kt "addent -password -p $user\@$realm -k $kvno -e $enc\n";
+            print $kt "$password\n";
+            print $kt "addent -password -p $user\@$realm -k $kvno -e $enc -s $salt\n";
+            print $kt "$password\n";
+            print $kt "addent -password -p $usr\@$realm -k $kvno -e $enc\n";
+            print $kt "$password\n";
+            print $kt "addent -password -p $usr\@$realm -k $kvno -e $enc -s $salt\n";
+            print $kt "$password\n";
+        }
+    }
+    print $kt "addent -password -p $upn -k $kvno -e $enc -s $salt\n";
+    print $kt "$password\n";
+    print $kt "addent -password -p $usr -k $kvno -e $enc\n";
+    print $kt "$password\n";
+}}
+print $kt "write_kt $ktfile.keytab\n";
+close $kt;
